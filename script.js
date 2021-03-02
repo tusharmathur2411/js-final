599917866cdee2d02433f25c4335ca772acc15a1
Glitch (hello-webpage) <none>
2021-03-02 11:09:25 +0000

    🌆🐁 Checkpoint ./script.js:29213475/887 ./home.js:29213475/55 ./cart.js:29213475/47 ./p.jroduct.js:29213475/37 ./product.js:29213475/11 ./index.html:29213475/23

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+import home from './home.js'
+import cart from './cart.js'
+import product from './product.js'
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
 const navitems = document.getElementsByClassName("navitem")
 
 for (let i = 0; i<navitems.length; i++ ) {
   console.log(navitems[i])
   navitems[i].addEventListener("click", (e) => {
-    console.log(e.target.id)
-    
+    const pathname = e.target.id;
+    console.log(pathname);
+    window.history.pushState(
+      {},
+      pathname,
+      window.location.origin + pathname
+    )
+    rootDiv.innerHTML = routes[pathname];
   })
 }
 
+const routes = {
+  '/' : home,
+  '/cart' : cart,
+  '/product' : product
+};
+
+const rootDiv = document.getElementById('root');
+rootDiv.innerHTML = routes[window.location.pathname];
+
+window.onpopstate = () => {
+  rootDiv.innerHTML = routes[window.location.pathname]
+}
