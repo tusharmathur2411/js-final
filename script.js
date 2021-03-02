7243ca08c5b8801c684e3483003f2c3df2888f50
Glitch (hello-webpage) <none>
2021-03-02 10:58:20 +0000

    💆🗼 Checkpoint ./index.html:29213475/908 ./style.css:29213475/46 ./script.js:29213475/252

--- script.js
+++ script.js
@@ -3,3 +3,13 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+const navitems = document.getElementsByClassName("navitem")
+
+for (let i = 0; i<navitems.length; i++ ) {
+  console.log(navitems[i])
+  navitems[i].addEventListener("click", (e) => {
+    console.log(e.target.id)
+    
+  })
+}
+
