2a220c3ec5e09df6c8d19f8b9975bdf2e6d76811
Glitch (hello-webpage) <none>
2021-03-02 11:20:28 +0000

    🏦🎾 Checkpoint ./home.js:29213475/978

--- home.js
+++ home.js
@@ -1,3 +1,35 @@
+let xhr = new XMLHttpRequest()
+
+const products = []
+
+xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      // let todos = document.getElementById("todos");
+      // todos.innerHTML = "";
+      products = JSON.parse(xhr.responseText);
+
+//       for (let id in res) {
+//         if (res[id]["imp"] === true) {
+//           todos.innerHTML += divTodo(res[id], id);
+//         }
+//       }
+
+//       for (let id in res) {
+//         if (res[id]["imp"] === false || res[id]["imp"] === undefined) {
+//           todos.innerHTML += divTodo(res[id], id);
+//         }
+//       }
+    }
+  };
+
+  xhr.open(
+    "GET",
+    "https://5d76bf96515d1a0014085cf9.mockapi.io/product",
+    true
+  );
+
+  xhr.send();
+
 export default `
 <div>
   Home
