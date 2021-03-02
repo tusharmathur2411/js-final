c6fb93cf2f549b7ccaff80d546b323994e23b61d
Glitch (hello-webpage) <none>
2021-03-02 14:06:23 +0000

    👯🎍 Checkpoint ./product.js:29213475/413

--- product.js
+++ product.js
@@ -1,33 +1,28 @@
 let xhr = new XMLHttpRequest()
 
+const productDiv = (pr) => `
+  
+  
+`
+
 const id = window.location.search.substr(1)
 
 xhr.onreadystatechange = () => {
   if (xhr.readyState == 4 && xhr.status == 200) {
-    let clothing = document.getElementById("clothing");
-    let accessories = document.getElementById("accessories");
+    // let clothing = document.getElementById("clothing");
+    // let accessories = document.getElementById("accessories");
     // todos.innerHTML = "";
     const res = JSON.parse(xhr.responseText);
     
     console.log(res[0])
 
-    for (let id in res) {
-      if (!res[id]["isAccessory"]) {
-        clothing.innerHTML += productDiv(res[id], id);
-      }
-    }
-
-    for (let id in res) {
-      if (res[id]["isAccessory"]) {
-        accessories.innerHTML += productDiv(res[id], id);
-      }
-    }
+    document.body.innerHTML = productDiv(res[id]);
   }
 };
 
 xhr.open(
   "GET",
-  "https://5d76bf96515d1a0014085cf9.mockapi.io/product" + id,
+  "https://5d76bf96515d1a0014085cf9.mockapi.io/product/" + id,
   true
 );
 
