3ff8da6b49896c567dc5816c946096a455eeba18
Glitch (hello-webpage) <none>
2021-03-02 11:32:24 +0000

    🍋🐂 Checkpoint ./home.js:29213475/297

--- home.js
+++ home.js
@@ -1,12 +1,14 @@
 let xhr = new XMLHttpRequest()
 
-const products = []
+let products = []
 
 xhr.onreadystatechange = () => {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      // let todos = document.getElementById("todos");
-      // todos.innerHTML = "";
-      products = JSON.parse(xhr.responseText);
+  if (xhr.readyState == 4 && xhr.status == 200) {
+    // let todos = document.getElementById("todos");
+    // todos.innerHTML = "";
+    products = JSON.parse(xhr.responseText);
+    
+    console.log(JSON.parse(xhr.responseText))
 
 //       for (let id in res) {
 //         if (res[id]["imp"] === true) {
@@ -19,18 +21,23 @@ xhr.onreadystatechange = () => {
 //           todos.innerHTML += divTodo(res[id], id);
 //         }
 //       }
-    }
-  };
+  }
+};
 
-  xhr.open(
-    "GET",
-    "https://5d76bf96515d1a0014085cf9.mockapi.io/product",
-    true
-  );
+xhr.open(
+  "GET",
+  "https://5d76bf96515d1a0014085cf9.mockapi.io/product",
+  true
+);
 
-  xhr.send();
+xhr.send();
 
-export default `
+console.log(products)
+
+const home = products.map(p => `
 <div>
-  Home
-</div>`
+  ${p.name}
+</div>
+`)
+
+export default home<h1;
