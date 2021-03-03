a8c6893c1f6ff55d5f68d3811b1b26bd48e8f091
Glitch (hello-webpage) <none>
2021-03-03 06:56:23 +0000

    🖐🛋 Checkpoint ./cart.js:29213475/607

--- cart.js
+++ cart.js
@@ -4,12 +4,23 @@ const cart = JSON.parse(localStorage.getItem("cart"))
 
 console.log(cart)
 
-// const productDiv = (pr) => `
-//   <img src="${pr["preview"]}" width="50%"/>
-  
-// `
+const cartDiv = (pr) => `
+  <div>
+   <img src="${pr["preview"]}" width="20%"/><br/>
+   <span>${pr["name"]}</span><br/>
+   <button>-</button>
+     <span>X${pr["count"]}</span><br/>   
+   <button>+</button>
+   <span>Amount: ${pr["price"] * pr["count"]}</span>
+  </div>
+ `
 
-document.body.innerHTML = cart.length;
+for (let pr of cart){
+  document.body.innerHTML += cartDiv(pr);  
+}
+
+
+
 
 // const id = window.location.search.substr(1)
 
