f72b9902e2bb2cd4794b08288dba52b13b99f964
Glitch (hello-webpage) <none>
2021-03-03 13:20:43 +0000

    🌓📌 Checkpoint ./script.js:29213475/1521 ./index.html:29213475/5 ./style.css:29213475/258

--- script.js
+++ script.js
@@ -17,19 +17,25 @@ const banners = ["https://cdn.glitch.com/cb384e5b-ed54-40c9-81c9-952d70798b31%2F
                  "https://cdn.glitch.com/cb384e5b-ed54-40c9-81c9-952d70798b31%2Fbanner-img-1.png?v=1614775711856",
                  "https://cdn.glitch.com/cb384e5b-ed54-40c9-81c9-952d70798b31%2Fbanner-img-2.png?v=1614775712494",
                  "https://cdn.glitch.com/cb384e5b-ed54-40c9-81c9-952d70798b31%2Fbanner-img-3.png?v=1614775715032"];
+
 let banner_id = 0
+document.getElementById("img-bnr").src = banners[banner_id];
 
 setInterval(() => {
+  document.getElementById("btn-"+banner_id).classList.remove("curr")
   banner_id = banner_id===3 ? 0 : banner_id+1;
-  document.getElementById("img-bnr").src = banners[banner_id]
+  document.getElementById("img-bnr").src = banners[banner_id];
+  document.getElementById("btn-"+banner_id).classList.add("curr")
 }, 5000)
 
 const buttons = document.getElementsByClassName("banner-btn")
 
 for (let _ of buttons) {
   _.addEventListener("click", (ev) => {
+    document.getElementById("btn-"+banner_id).classList.remove("curr")
     banner_id = parseInt(ev.target.id.slice(-1))
     document.getElementById("img-bnr").src = banners[banner_id]
+    document.getElementById("btn-"+banner_id).classList.add("curr")
   })
 }
 
