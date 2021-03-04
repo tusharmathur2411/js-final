1d8e7a5daf2090748c3a8e7c7c82ebd3ddd0f568
Glitch (hello-webpage) <none>
2021-03-04 18:52:32 +0000

    🎖💠 Checkpoint ./style.css:29213475/38 ./nav.js:29213475/1080 ./index.html:29213475/53 ./product.html:29213475/28 ./cart.html:29213475/42 ./nav.css:29213475/150

--- nav.js
+++ nav.js
@@ -7,9 +7,37 @@ const navbar = `
   </i><input type="text" placeholder="Search"/>
   <a href="./cart.html"><img src="https://img.icons8.com/fluent/48/000000/fast-cart.png"/><span id="cart-count"></span></a>
 `
-
+const footer = `<div>
+	<p class="footer-heading">Online Store</p>
+	<a href="/" class="footer-link">Men Clothing</a>
+	<a href="/" class="footer-link">Women Clothing</a>
+	<a href="/" class="footer-link">Men Accessories</a>
+	<a href="/" class="footer-link">Women Accessories</a>
+</div>
+<div>
+	<p class="footer-heading">Helpful Links</p>
+	<a href="/" class="footer-link">Home</a>
+	<a href="/" class="footer-link">About</a>
+	<a href="/" class="footer-link">Contact</a>
+</div>
+<div>
+	<p class="footer-heading">Partners</p>
+	<a href="/" class="footer-link">Zara</a>
+	<a href="/" class="footer-link">Pantaloons</a>
+	<a href="/" class="footer-link">Levis</a>
+	<a href="/" class="footer-link">UCB</a>
+	<a href="/" class="footer-link">+ Many More</a>
+</div>
+<div>
+	<p class="footer-heading">Address</p>
+	<p href="/" class="footer-link">Building 101</p>
+	<p href="/" class="footer-link">Central Avenue</p>
+	<p href="/" class="footer-link">LA - 902722</p>
+	<p href="/" class="footer-link">United States</p>
+</div>`
 
 document.getElementById("navbar").innerHTML = navbar
+document.getElementById("footer").innerHTML = footer
 
 let cart = localStorage.getItem("cart");
 localStorage.setItem("cart", (cart===null || cart===""?  JSON.stringify([]): cart));
