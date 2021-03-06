031e70b4a90c957d7cab240c271f4cb51f34ba15
Glitch (hello-webpage) <none>
2021-03-06 16:34:00 +0000

    😀🎷 Checkpoint ./cart.js:29213475/36

--- cart.js
+++ cart.js
@@ -50,7 +50,7 @@ const placeOrder = () => {
 
   xhr.open(
     "POST",
-    "https://5d76bf96515d1a0014085cf9.mockapi.io/order",
+    "https://cors-anywhere.herokuapp.com/https://5d76bf96515d1a0014085cf9.mockapi.io/order",
     true
   );
 
